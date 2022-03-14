05d86129af010677745d44c440b5c31bb8447641
Guilherme <jogos.coisas@gmail.com>
2022-03-14 12:25:45 -0300

    gitignore bug fix

---
+++ dart/lib/iniciante.dart
@@ -0,0 +1,35 @@
+///
+/// concatenar os 2 parametros
+/// ex: str1: moran, str2: go
+/// r: morango
+///
+String concatenar(String str1, String str2){
+  return "";
+}
+
+///
+/// gerar um array separando as palavras por espaço
+/// ex: mercado livre
+/// r: ['mercado', 'livre']
+///
+List<String> gerarArraySeparandoTextoPorEspaco(String str){
+  return [];
+}
+
+///
+/// verificar se um texto tem mais de 10 caracteres ignorando os espaços
+/// ex: eu fui no mercado comprar pão
+/// r: true
+///
+bool contarCaracteresIgnorandoEspaco(String str){
+  return true;
+}
+
+///
+/// adicionar item no array
+/// ex: array: ['a', 'b'], str: 'h'
+/// r: ['a', 'b', 'h']
+///
+List<String> adicionarItemNoArray(List<String> array, String str){
+  return [];
+}
---
+++ dart/lib/intermediario.dart
@@ -0,0 +1,47 @@
+///
+/// contar quantos 'a' tem em uma frase (maiúsculas e Minúsculas)
+/// ex: eu fui no mercado comprar pão
+/// r: 3
+///
+int contarCaracteresA(String str){
+  return 0;
+}
+
+///
+/// contar quantos 'a' e 'o' tem em uma frase (maiúsculas e Minúsculas)
+/// retornar true caso tenha a mesma quantidade de 'a' e 'o'
+/// ex: eu fui no mercado comprar pão -> a: 3 o: 4
+/// r: false
+///
+bool contarECompararCaracteresAO(String str){
+  return false;
+}
+
+///
+/// retornar a posição dos caracteres 'a' e 'o'
+/// ex: eu fui no mercado comprar pão
+/// r: [8, 14, 16 ,19 ,23 ,28]
+///
+List<int> obterIndexAO(String str){
+  return [];
+}
+
+///
+/// Inverta as palavras que tem mais de 4 caracteres
+/// ex: oi menu nome é guilherme
+/// r: oi meu nome é emrehliug
+///
+String inverterParteDaString(String str){
+  return "";
+}
+
+///
+/// mascara até os 4 ultimos caracteres ignorando os espaços
+/// ex: 123456780
+/// r: #####6780
+/// ex: 1234 56780
+/// r: #### #6780
+///
+String mascararString(String str){
+  return "";
+}
