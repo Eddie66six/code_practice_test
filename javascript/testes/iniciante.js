bbebeeaf7b5a0b166a7818df2a9b01223c711e03
Guilherme <jogos.coisas@gmail.com>
2022-03-17 10:03:09 -0300

    bug fix javascript

--- javascript/testes/iniciante.js
+++ javascript/testes/iniciante.js
@@ -1,64 +1,64 @@
-var assert = require('assert');
-var shared = require('./shared.js');
-var objTest = require('../codes/iniciante.js');
+var assert = require("assert");
+var shared = require("./shared.js");
+var objTest = require("../codes/iniciante.js");
 
-describe('concatenar', function() {
-    it('teste 1', function() {
-        let str1 = 'um macaco';
-        let str2 = ' come banana';
+describe("concatenar", function() {
+    it("teste 1", function() {
+        let str1 = "um macaco";
+        let str2 = " come banana";
         let strR = str1 + str2;
-        assert.ok(objTest.concatenar(str1, str2) == strR, shared.formatarErro(str1 + ',' + str2, strR.toString()));
+        assert.ok(objTest.concatenar(str1, str2) == strR, shared.formatarErro(str1 + "," + str2, strR.toString()));
     })
 })
 
-describe('separar palavras', function() {
-    it('teste 1', function() {
-        let str = 'Desta maneira, o fenômeno da Internet não pode mais se dissociar do processo de comunicação como um todo';
-        let strR = ['Desta', 'maneira,', 'o', 'fenômeno', 'da', 'Internet', 'não', 'pode', 'mais', 'se', 'dissociar', 'do', 'processo', 'de', 'comunicação', 'como', 'um', 'todo'];
-        assert.ok(objTest.gerarArraySeparandoTextoPorEspaco(str).join('-') == strR.join('-'), shared.formatarErro(str, strR.join(',')));
+describe("separar palavras", function() {
+    it("teste 1", function() {
+        let str = "Desta maneira, o fenômeno da Internet não pode mais se dissociar do processo de comunicação como um todo";
+        let strR = ["Desta", "maneira,", "o", "fenômeno", "da", "Internet", "não", "pode", "mais", "se", "dissociar", "do", "processo", "de", "comunicação", "como", "um", "todo"];
+        assert.ok(objTest.gerarArraySeparandoTextoPorEspaco(str).join("-") == strR.join("-"), shared.formatarErro(str, strR.join(",")));
     })
 })
 
-describe('contar caracteres ignorando espaço', function() {
-    it('teste 1', function() {
-        let str = 'Desta maneira, o fenômeno da Internet não pode mais se dissociar do processo de comunicação como um todo';
+describe("contar caracteres ignorando espaço", function() {
+    it("teste 1", function() {
+        let str = "Desta maneira, o fenômeno da Internet não pode mais se dissociar do processo de comunicação como um todo";
         let strR = true;
         assert.ok(objTest.contarCaracteresIgnorandoEspaco(str) == strR, shared.formatarErro(str, strR.toString()));
     })
 })
 
-describe('adicionar item a um array', function() {
-    it('teste 1', function() {
-        let array = ['a', 'b'];
-        let str = 'hhh';
-        let strR = ['a', 'b', 'hhh'];
-        assert.ok(objTest.adicionarItemNoArray(array, str).join('-') == strR.join('-'),
-            shared.formatarErro(array.join(',') + '+' + str, strR));
+describe("adicionar item a um array", function() {
+    it("teste 1", function() {
+        let array = ["a", "b"];
+        let str = "hhh";
+        let strR = ["a", "b", "hhh"];
+        assert.ok(objTest.adicionarItemNoArray(array, str).join("-") == strR.join("-"),
+            shared.formatarErro(array.join(",") + "+" + str, strR));
     })
 })
 
-describe('remover item do array', function() {
-    it('teste 1', function() {
-        let array = ['a', 'b', 'hhh'];
-        let str = 'hhh';
-        let strR = ['a', 'b'];
-        assert.ok(objTest.removerItemDoArray(array, str).join('-') == strR.join('-'),
-            shared.formatarErro(array.join(',') + '+' + str, strR.join(',')));
+describe("remover item do array", function() {
+    it("teste 1", function() {
+        let array = ["a", "b", "hhh"];
+        let str = "hhh";
+        let strR = ["a", "b"];
+        assert.ok(objTest.removerItemDoArray(array, str).join("-") == strR.join("-"),
+            shared.formatarErro(array.join(",") + "+" + str, strR.join(",")));
     })
 })
 
-describe('captalizar simples', function() {
-    it('teste 1', function() {
-        let str = 'guilherme';
-        let strR = 'Guilherme';
+describe("captalizar simples", function() {
+    it("teste 1", function() {
+        let str = "guilherme";
+        let strR = "Guilherme";
         assert.ok(objTest.captalizarSimples(str) == strR, shared.formatarErro(str, strR));
     })
 })
 
-describe('captalizar avancada', function() {
-    it('teste 1', function() {
-        let str = 'guilherme vai para o mercado';
-        let strR = 'Guilherme Vai Para O Mercado';
+describe("captalizar avancada", function() {
+    it("teste 1", function() {
+        let str = "guilherme vai para o mercado";
+        let strR = "Guilherme Vai Para O Mercado";
         assert.ok(objTest.captalizarAvancada(str) == strR, shared.formatarErro(str, strR));
     })
 })
--- javascript/testes/intermediario.js
+++ javascript/testes/intermediario.js
@@ -1,45 +1,45 @@
-var assert = require('assert');
-var shared = require('./shared.js');
-var objTest = require('../codes/intermediario.js');
+var assert = require("assert");
+var shared = require("./shared.js");
+var objTest = require("../codes/intermediario.js");
 
-describe('contar caracteres', function() {
-    it('teste 1', function() {
-        let str = 'um macaco come banana';
+describe("contar caracteres", function() {
+    it("teste 1", function() {
+        let str = "um macaco come banana";
         let strR = 5;
         assert.ok(objTest.contarCaracteresA(str) == strR, shared.formatarErro(str, strR.toString()));
     })
 })
 
-describe('contar caracteres repetidos', function() {
-    it('teste 1', function() {
-        let str = 'Desta maneira, o fenômeno da Internet não pode mais se dissociar do processo de comunicação como um todo';
+describe("contar caracteres repetidos", function() {
+    it("teste 1", function() {
+        let str = "Desta maneira, o fenômeno da Internet não pode mais se dissociar do processo de comunicação como um todo";
         let strR = false;
         assert.ok(objTest.contarECompararCaracteresAO(str) == strR, shared.formatarErro(str, strR));
     })
 })
 
-describe('index char', function() {
-    it('teste 1', function() {
-        let str = 'Desta maneira, o fenômeno da Internet não pode mais se dissociar do processo de comunicação como um todo';
+describe("index char", function() {
+    it("teste 1", function() {
+        let str = "Desta maneira, o fenômeno da Internet não pode mais se dissociar do processo de comunicação como um todo";
         let strR = [4, 7, 12, 27, 48, 62, 87];
-        assert.ok(objTest.obterIndexAO(str).join('-') == strR.join('-'), shared.formatarErro(str, strR.join(',')));
+        assert.ok(objTest.obterIndexAO(str).join("-") == strR.join("-"), shared.formatarErro(str, strR.join(",")));
     })
 })
 
-describe('inversão de string', function() {
-    it('teste 1', function() {
-        let str = 'um macaco come banana';
-        let strR = 'um ocacam come ananab';
+describe("inversão de string", function() {
+    it("teste 1", function() {
+        let str = "um macaco come banana";
+        let strR = "um ocacam come ananab";
         assert.ok(objTest.inverterParteDaString(str) == strR, shared.formatarErro(str, strR));
     })
-    it('teste 2', function() {
-        let str = 'Desta maneira, o fenômeno da Internet não pode mais se dissociar do processo de comunicação como um todo';
-        let strR = 'atseD ,arienam o onemônef da tenretnI não pode mais se raicossid do ossecorp de oãçacinumoc como um todo';
+    it("teste 2", function() {
+        let str = "Desta maneira, o fenômeno da Internet não pode mais se dissociar do processo de comunicação como um todo";
+        let strR = "atseD ,arienam o onemônef da tenretnI não pode mais se raicossid do ossecorp de oãçacinumoc como um todo";
         assert.ok(objTest.inverterParteDaString(str) == strR, shared.formatarErro(str, strR));
     })
-    it('teste 3', function() {
-        let str = 'só mais esse teste para garantir';
-        let strR = 'só mais esse etset para ritnarag';
+    it("teste 3", function() {
+        let str = "só mais esse teste para garantir";
+        let strR = "só mais esse etset para ritnarag";
         assert.ok(objTest.inverterParteDaString(str) == strR, shared.formatarErro(str, strR));
     })
 })
